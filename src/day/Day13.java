8ba9f2f2060a4a4079b5747296652a3ba915712e
picressant <pierre.cressant@gmail.com>
2020-12-15 21:37:31 +0100

    feat : resolved day13 - part two HUGE THANKS ROSETTA CODE !

--- src/day/Day13.java
+++ src/day/Day13.java
@@ -1,12 +1,15 @@
 package day;
 
+import javafx.util.Pair;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 public class Day13 {
 
@@ -15,7 +18,7 @@ public class Day13 {
             File myObj = new File("/home/pierre/work/perso/advent.input");
             Scanner myReader = new Scanner(myObj);
 
-            List<Integer> buses = new ArrayList<>();
+            List<String> buses = new ArrayList<>();
             int timestamp = 0;
 
             while (myReader.hasNextLine()) {
@@ -24,23 +27,31 @@ public class Day13 {
                     timestamp = Integer.parseInt(data);
                 }
                 else {
-                    buses = Arrays.stream(data.split(",")).filter(d -> !d.equals("x")).map(Integer::parseInt).collect(Collectors.toList());
+                    buses = Arrays.asList(data.split(","));
                 }
             }
 
-            int nextBus = 0;
-            int nextTimeStamp = 0;
-
-            for (Integer bus : buses) {
-                int busNext = Math.floorDiv(timestamp, bus) * bus + bus;
-                if (nextBus == 0 || nextTimeStamp > busNext) {
-                    nextBus = bus;
-                    nextTimeStamp = busNext;
+            List<Pair<Long, Long>> busesAndModulo = new ArrayList<>();
+            for (int i = 0; i < buses.size(); i++) {
+                if (buses.get(i).equals("x")) {
+                    continue;
                 }
+
+                long bus = Long.parseLong(buses.get(i));
+                long mod = (i == 0) ? 0 : bus - i;
+                while (mod < 0)
+                    mod += bus;
+                Pair<Long, Long> p = new Pair<>(bus, mod);
+
+                busesAndModulo.add(p);
             }
 
-            System.out.println("Next bus: " + nextBus + " - timestamp: " + nextTimeStamp);
-            System.out.println("Res: " + ((nextTimeStamp - timestamp) * nextBus));
+            System.out.println(busesAndModulo);
+
+            Long[] modulos = busesAndModulo.stream().map(Pair::getValue).toArray(Long[]::new);
+            Long[] div = busesAndModulo.stream().map(Pair::getKey).toArray(Long[]::new);
+
+            System.out.println("res: " + chineseRemainder(div, modulos));
 
             myReader.close();
 
@@ -49,5 +60,43 @@ public class Day13 {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    public static long chineseRemainder(Long[] n, Long[] a) {
+
+        long prod = stream(n).reduce(1L, (i, j) -> i * j);
+
+        long p, sm = 0;
+        for (int i = 0; i < n.length; i++) {
+            p = prod / n[i];
+            sm += a[i] * mulInv(p, n[i]) * p;
+        }
+        return sm % prod;
+    }
+
+    private static long mulInv(long a, long b) {
+        long b0 = b;
+        long x0 = 0;
+        long x1 = 1;
+
+        if (b == 1) {
+            return 1;
+        }
+
+        while (a > 1) {
+            long q = a / b;
+            long amb = a % b;
+            a = b;
+            b = amb;
+            long xqx = x1 - q * x0;
+            x1 = x0;
+            x0 = xqx;
+        }
+
+        if (x1 < 0) {
+            x1 += b0;
+        }
+
+        return x1;
     }
 }
